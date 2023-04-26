abe3d75e5f822e5b270446491d27fd054990e7b4
okekevicktur <okekevicktur@gmail.com>
2023-04-26 18:42:48 +0100

    Basically finished the mobile view responsiveness

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello World!</h1>
+      <Header />
     </>
   );
 }
